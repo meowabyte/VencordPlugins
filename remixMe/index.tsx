d38953bab234440f09f0f23855230023ee0055f9
meowabyte <pojg77g1@kbb.anonaddy.me>
2025-06-14 23:22:19 +0200

    did a little oopsie on remixMe (again)

--- remixMe/index.tsx
+++ remixMe/index.tsx
@@ -4,20 +4,25 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { addMessagePreSendListener, type MessageExtra, type MessageObject, type MessageSendListener, removeMessagePreSendListener } from "@api/MessageEvents";
-import { EquicordDevs } from "@utils/constants";
+import {
+    addMessagePreSendListener,
+    MessageSendListener,
+    removeMessagePreSendListener,
+} from "@api/MessageEvents";
 import definePlugin from "@utils/types";
 
-import type { UploadWithRemix } from "./types";
-
-
-const handleMessage: MessageSendListener = (_: string, __: MessageObject, ex: MessageExtra) =>
-    ex.uploads && (ex.uploads as UploadWithRemix[]).forEach(att => att.isRemix = true);
+// @ts-expect-error isRemix isnt on the upload[] type yet
+const handleMessage: MessageSendListener = (_, __, ex) =>
+    ex.uploads && ex.uploads.forEach((att) => (att.isRemix = true));
 
 export default definePlugin({
     name: "RemixMe",
     description: "Turns every single message with attachment to have remix tag",
-    authors: [EquicordDevs.meowabyte],
-    start: () => addMessagePreSendListener(handleMessage),
-    stop: () => removeMessagePreSendListener(handleMessage)
+    authors: [{ id: 105170831130234880n, name: "kb" }],
+    start() {
+        addMessagePreSendListener(handleMessage);
+    },
+    stop() {
+        removeMessagePreSendListener(handleMessage);
+    },
 });
